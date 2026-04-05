d824f3d162dd558a584ef004c61c5b7d0d5cbf42
Đinh Trung Kiên <110721139+ryzen30xx@users.noreply.github.com>
2026-04-05 16:00:14 +0700

    [fix] fix Secure connection when post to release product

--- webapp/playwright.config.ts
+++ webapp/playwright.config.ts
@@ -2,7 +2,7 @@
 
 import { defineConfig } from '@playwright/test';
 
-const baseURL = process.env.PLAYWRIGHT_BASE_URL ?? 'https://127.0.0.1:3000';
+const baseURL = process.env.PLAYWRIGHT_BASE_URL ?? 'https://127.0.0.1:3443';
 
 export default defineConfig({
   reporter: 'list',
