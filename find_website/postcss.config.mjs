88d0e3e109858d963dfe3db03e5a2f8c14d7bb40
Đinh Trung Kiên <110721139+ryzen30xx@users.noreply.github.com>
2026-04-10 23:20:33 +0700

    [Update] remove find_website after standalone split

--- find_website/postcss.config.mjs
+++
@@ -1,9 +0,0 @@
-/* Copyright (c) 2026 Đinh Trung Kiên. All rights reserved. */
-
-const config = {
-  plugins: {
-    "@tailwindcss/postcss": {},
-  },
-};
-
-export default config;
--- find_website/src/lib/utils.ts
+++
@@ -1,8 +0,0 @@
-/* Copyright (c) 2026 Đinh Trung Kiên. All rights reserved. */
-
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
